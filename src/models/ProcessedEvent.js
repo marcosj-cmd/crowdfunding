a94ebe58594744706290775a54ffe5b29988acea
marcos <marcosj@uoc.edu>
2025-10-09 19:20:49 +0200

    frontend

--- src/models/ProcessedEvent.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ProcessedEventSchema = new mongoose.Schema(
-  { _id: { type: String, required: true } }, // txHash-logIndex
-  { timestamps: true }
-);
-
-export default mongoose.model("ProcessedEvent", ProcessedEventSchema);
- 
